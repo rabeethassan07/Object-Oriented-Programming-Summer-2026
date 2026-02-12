db6f0e994e0ad508592d3eb7ee4d120e075b6b80
Syed Rabeet Hassan <rabeethassan07@gmail.com>
2026-02-12 22:38:10 +0500

    C++ Programming

---
+++ Codes/Sum of Real Parts.cpp
@@ -0,0 +1,53 @@
+ #include<iostream>
+ using namespace std;
+
+ //Forward declaration
+ class Complex;
+
+ class Calculator
+ {
+    public:
+    int add (int a, int b){
+        return(a+b);
+    }
+    int sumRealComplex(Complex,Complex);
+    int sunCompCopmlex(Complex,Complex);
+ };
+ class Complex
+ {
+    int a,b;
+    friend int Calculator :: sumRealComplex(Complex , Complex );
+    friend int Calculator :: sumCompComplex(Complex ,Complex); 
+    public:
+    void setnumber (int n1, int n2)
+	{
+        a = n1;
+        b = n2;
+    }
+    void printnumber()
+	{
+    	cout<<"Your number is "<<a<<" + "<<b<<"i"<<endl;
+    }
+ };
+ int Calculator :: sumRealComplex(Complex o1,Complex o2)
+ {
+    return (o1.a + o2.a); 
+}
+ int Calculator :: sumCompComplex(Complex o1,Complex o2)
+ {
+    return (o1.b + o2.b); 
+}
+int main()
+{
+	Complex o1, o2;
+    o1.setnumber(1,4);
+    o2.setnumber(5,7);
+    
+    Calculator calc;
+    int res = calc.sumRealComplex(o1,o2);
+    cout<<"The sum of real partof o1 and o2 is "<<res<<endl;
+    int resc = calc.sunCompCopmlex (o1,o2);
+    cout<<"The sum of complex part of o1 and o2 is "<<resc<<endl;
+
+    return 0; 
+}
