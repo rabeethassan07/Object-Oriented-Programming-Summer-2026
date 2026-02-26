74c1fe9d2f44563204f504dc08b5cdee0c34ef5d
Syed Rabeet Hassan <rabeethassan07@gmail.com>
2026-02-26 21:15:14 +0500

    C++ Programming

---
+++ Codes/3-Digit Identifier.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    
+    cout<<"Enter n : ";
+    cin>>n;
+
+    if(n>99 and n<1000)
+	{
+        cout<<"3 digit number";
+    }
+    else
+	{
+        cout<<"Not a 3 digit number";
+    }
+    return 0;
+}
